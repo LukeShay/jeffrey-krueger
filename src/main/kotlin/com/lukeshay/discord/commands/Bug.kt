93ea6d224095d88722867b3f17e916fb7bf8825a
Luke Shay <shay.luke17@gmail.com>
2021-02-25 22:24:43 -0600

    added failure message

---
+++ src/main/kotlin/com/lukeshay/discord/commands/Bug.kt
@@ -0,0 +1,22 @@
+package com.lukeshay.discord.commands
+
+import com.lukeshay.discord.enums.Environment
+import com.lukeshay.discord.enums.FeatureStatus
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class Bug @Autowired constructor(environment: Environment) :
+    Command(
+        "bug",
+        "I will reply with the bug report link.",
+        true,
+        FeatureStatus.RELEASE,
+        environment
+    ) {
+    override fun run(event: GuildMessageReceivedEvent) {
+        event.message.reply("https://github.com/LukeShay/jeffrey-krueger/issues/new?assignees=LukeShay&labels=bug%2C+awaiting+triage&template=bug_report.md&title=%5BBUG%5D+Your+title+here")
+            .queue()
+    }
+}
